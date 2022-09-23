6daedce8c2d966710699aa3cae9bddbd3265ff13
kiara-bermudez <kiarabermudez.10@gmail.com>
2022-09-23 17:11:07 -0500

    Implemented ability to add new items to list.

--- app/src/main/java/com/example/wishlist/MainActivity.kt
+++ app/src/main/java/com/example/wishlist/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.wishlist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -14,11 +17,23 @@ class MainActivity : AppCompatActivity() {
         // Lookup the recyclerview in activity layout
         val rvWishes = findViewById<View>(R.id.recyclerView) as RecyclerView
         // Initialize contacts
-        val wishes = listOf<Wish>(
-            Wish("Coke", 2.00, "https://www.coca-cola.com/")
-        )
+        var wishes = ArrayList<Wish>()
         // Create adapter passing in the sample user data
         val adapter = WishAdapter(wishes)
+
+        val itemNameET = findViewById<EditText>(R.id.itemNameEt)
+        val itemPriceET = findViewById<EditText>(R.id.itemPriceEt)
+        val itemUrlET = findViewById<EditText>(R.id.urlEt)
+        val submitBtn = findViewById<Button>(R.id.submitBtn)
+        submitBtn.setOnClickListener {
+            // replace this line with wherever you get new records
+            val newWish = Wish(itemNameET.text.toString(), itemPriceET.text.toString().toDouble(), itemUrlET.text.toString())
+            // update the existing list
+            wishes.add(newWish)
+            // Notify the adapter that you have changed list
+            adapter.notifyDataSetChanged()
+        }
+
         // Attach the adapter to the recyclerview to populate items
         rvWishes.adapter = adapter
         // Set layout manager to position the items
